460c88529495a3755c044bdfa2cb7b95e6c87d5d
Rachel S. Lee <rslee0025@gmail.com>
2020-06-09 01:47:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
   
-  epic_tragedy.update(epic_tragedy) { |status, "alive"| "dead"}
+  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   
